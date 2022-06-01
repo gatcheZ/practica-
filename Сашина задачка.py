241e3b86aff0d6c5cc4964bb298869cac408c40d
Мелькумова <murlakukurla@gmail.com>
2022-06-01 13:43:42 +0300

    qwe

--- Сашина задачка.py
+++ Сашина задачка.py
@@ -1,16 +1,15 @@
 name_tovar = 'Ноутбук'
 coin = 0
 
-
 popupka = {
     "Попыт": 6,
     "Микроволновка": 8,
-    "Ноутбук": 2
+    "Ноутбук": 82
 }
 object = {
     "Ноутбук": {
         "Стоимость товара": 99000,
-        "Остаток на складе": 5,
+        "Остаток на складе": 0,
         "Категория товара": "Электроника"
     },
     "Смартфон": {
@@ -24,44 +23,83 @@ object = {
         "Категория товара": "Бытовая техника"
     }
 }
+
+
 def 云克苏(spisok: dict):
     for price, amount in spisok.items():
-        print("朗道" * 20)
+        print("." * 20)
         print(f"Наименование товара {price!r}")
-        print("화준" * 20)
+        print("-" * 20)
         for object_name, kinds in amount.items():
             print(f"{object_name} - {kinds}")
+
 
 def bubuyka(spisok: dict, tovar: str):
     if tovar in spisok:
         global coin
-        coin = coin + spisok[tovar]["Стоимость товара"]
-        spisok[tovar]["Остаток на складе"] = spisok[tovar]["Остаток на складе"] - 1
-        print(f"В кассе есть - {coin} руб.")
+        if spisok[tovar]["Остаток на складе"] <= 0:
+            print("Товара нет на складе")
+        else:
+            coin = coin + spisok[tovar]["Стоимость товара"]
+            spisok[tovar]["Остаток на складе"] = spisok[tovar]["Остаток на складе"] - 1
+            print(f"В кассе есть - {coin} руб.")
     else:
         print("Такого товара нет")
 
-def popupki(chto_pokupaem:dict,spisok: dict):
+
+def popupki(chto_pokupaem: dict, spisok: dict):
     global coin
     for kind, count in chto_pokupaem.items():
         if kind in spisok.keys():
-            if spisok[kind]["Остаток на складе"] < chto_pokupaem[count]:
-                coin += spisok[kind] * chto_pokupaem[kind]["Стоимость товара"]
+            if spisok[kind]["Остаток на складе"] < count:
+                in_stock = spisok[kind]["Остаток на складе"]
+                price = count * in_stock
+                coin += spisok[kind]["Остаток на складе"] * spisok[kind]["Стоимость товара"]
                 spisok[kind]["Остаток на складе"] = 0
+                print(f"Вы купили {kind}, {in_stock} предмета, потому что больше нет, и стоило это {price} денег")
             else:
-                coin += count * chto_pokupaem[kind]["Стоимость товара"]
-                spisok[count] -= count
+                price_1 = count * spisok[kind]["Стоимость товара"]
+                coin += price_1
+                spisok[kind]["Остаток на складе"] -= count
+                print(f"Вы купили {kind}, {count} предмета, и стоило это {price_1} денег")
 
-            print(coin)
-            云克苏(spisok)
+            # 云克苏(spisok)
+            print(f"В кассе у нас {coin} денег ")
         else:
-            print("у нас такого нет, иди отсюда")
-popupki(popupka, object)
+            print(f"Ты не сможешь купить {kind}, у нас такого нет, иди отсюда")
 
 
+# bubuyka(object, "qwe")
+# print(object)
+# popupki(popupka, object)
 
 
-# if __name__ == 'main':
-    # bubuyka(object, name_tovar)
-    # 云克苏(object)
-    # popupki(popupka, object)
+def 妓女(spisok: dict, tovar: str):
+    if tovar not in spisok:
+        print("Иди отсюда пес, такого нет")
+        return
+    for name, glossary in spisok.items():
+        if spisok[tovar]["Категория товара"] == glossary["Категория товара"]:
+            analog = dict()
+            kat = glossary["Категория товара"]
+            if spisok[tovar]["Стоимость товара"] > glossary["Стоимость товара"]:
+                print(f"Товар {name} дешевле чем {tovar}, он также относится к категории {kat}")
+                analog[name] = glossary
+                return 云克苏(analog)
+            else:
+                print(f"Нет ничего дешевле {tovar} в категории {kat}")
+
+
+# 妓女(object, "Ноутбук")
+
+def refill(spisok: dict, tovarui: dict):
+    for kind, count in tovarui.items():
+        if kind in spisok.keys():
+            spisok[kind]["Остаток на складе"] += count
+        else:
+            spisok[kind] = dict()
+            spisok[kind]["Остаток на складе"] = count
+    return 云克苏(spisok)
+
+
+refill(object, popupka)
